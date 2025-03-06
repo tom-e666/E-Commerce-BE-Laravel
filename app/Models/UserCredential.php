db501a41da6b6238004c3881624c0c1823226e23
Admin <tptien.a1.c3tqcap@gmail.com>
2025-03-06 12:26:31 +0700

    nothingf

--- app/Models/UserCredential.php
+++ app/Models/UserCredential.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -13,31 +12,57 @@ use Tymon\JWTAuth\Contracts\JWTSubject;
 class UserCredential extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
+
     protected $table = 'user_credentials';
-    protected $fillable = array(
+
+    protected $fillable = [
         'username',
         'email',
         'phone',
         'password',
         'email_verified',
         'phone_verified',
-    );
+    ];
+
     protected $hidden = [
         'password'
     ];
-    protected $casts =
-    [
+
+    protected $casts = [
         'email_verified' => 'boolean',
         'phone_verified' => 'boolean',
     ];
+
+    /**
+     * Set the user's password.
+     *
+     * @param  string  $value
+     * @return void
+     */
     public function setPasswordAttribute($value): void
     {
-        $this->attributes['password'] = Hash::make($value);
+        if (!Hash::needsRehash($value)) {
+            $this->attributes['password'] = Hash::make($value);
+        } else {
+            $this->attributes['password'] = $value;
+        }
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
     public function getJWTIdentifier()
     {
         return $this->getKey();
     }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
     public function getJWTCustomClaims(): array
     {
         return [];
