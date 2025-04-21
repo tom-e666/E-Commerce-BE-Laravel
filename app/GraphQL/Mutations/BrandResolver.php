0c6238a2676fbaf4c20a5b4feb04ded59ca562ca
Jhandf <nguyenduy2004.d@gmail.com>
2025-04-21 10:21:31 +0700

    add update brand

--- app/GraphQL/Mutations/BrandResolver.php
+++ app/GraphQL/Mutations/BrandResolver.php
@@ -38,4 +38,27 @@ final readonly class BrandResolver
             return $this->error($e->getMessage(), 500);
         }
     }
+    public function updateBrand($_, array $args)
+    {
+        $validator = Validator::make($args, [
+            'id' => 'required|exists:brands,id',
+            'name' => 'required|string|unique:brands,name,' . $args['id'],
+        ]);
+
+        if ($validator->fails()) {
+            return $this->error($validator->errors()->first(), 400);
+        }
+
+        try {
+            $brand = Brand::find($args['id']);
+            $brand->update([
+                'name' => $args['name'],
+            ]);
+            return $this->success([
+                'brand' => $brand,
+            ], 'success', 200);
+        } catch (Exception $e) {
+            return $this->error($e->getMessage(), 500);
+        }
+    }
 }
