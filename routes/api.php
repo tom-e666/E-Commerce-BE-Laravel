e4401532ea61fe1c6f9ecda3bf47c256f7c2a798
Jhandf <nguyenduy2004.d@gmail.com>
2025-04-24 00:35:12 +0700

    Refactor

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\AdminOrderController;
-use App\Http\Controllers\ReviewController;
-use App\Http\Controllers\AdminReportController;
-use App\Http\Controllers\PromotionController;
-use App\Http\Controllers\ShippingController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
