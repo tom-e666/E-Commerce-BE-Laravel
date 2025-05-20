33b4718336037251809e685ae1bd53a46dc4732f
Admin <tptien.a1.c3tqcap@gmail.com>
2025-05-20 11:25:46 +0700

    add new

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\Product;
 use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
 /**
  * @extends \Illuminate\Database\Eloquent\Factories.Factory<\App\Models\Product>
  */
